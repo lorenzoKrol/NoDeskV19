dc860ed1057cada53168c40625e64349e0e05364
lorenzoKrol <42969133+lorenzoKrol@users.noreply.github.com>
2021-04-30 13:09:58 +0200

    Update Notifications_Service.cs

--- NoDeskV19/TGG_Service/Notifications_Service.cs
+++ NoDeskV19/TGG_Service/Notifications_Service.cs
@@ -43,12 +43,12 @@ namespace TGG_Service
                 try
                 {
                     //SMTP mailserver 
-                    SmtpClient mySmtpClient = new SmtpClient("mail.inholland-project.nl");
+                    SmtpClient mySmtpClient = new SmtpClient("");
 
                     // set smtp-client with basicAuthentication
                     mySmtpClient.UseDefaultCredentials = false;
                     System.Net.NetworkCredential basicAuthenticationInfo = new
-                       System.Net.NetworkCredential("nodesk@inholland-project.nl", "Pindakaas01");
+                       System.Net.NetworkCredential("");
                     mySmtpClient.Credentials = basicAuthenticationInfo;
 
                     // add from,to mailaddresses
@@ -184,12 +184,12 @@ namespace TGG_Service
                 try
                 {
                     //SMTP mailserver 
-                    SmtpClient mySmtpClient = new SmtpClient("mail.inholland-project.nl");
+                    SmtpClient mySmtpClient = new SmtpClient("");
 
                     // set smtp-client with basicAuthentication
                     mySmtpClient.UseDefaultCredentials = false;
                     System.Net.NetworkCredential basicAuthenticationInfo = new
-                       System.Net.NetworkCredential("nodesk@inholland-project.nl", "Pindakaas01");
+                       System.Net.NetworkCredential("");
                     mySmtpClient.Credentials = basicAuthenticationInfo;
 
                     // add from,to mailaddresses
